541e7b1987b7b2ac6fd1bf13f9c468fad919e84d
NobilisPerfectum <148438143+NobilisPerfectum@users.noreply.github.com>
2025-08-29 23:19:01 +0530

    2Pokemon files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'pokemon-root',
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'pokemon';
+  title = '2pokemon';
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,2 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { PokemonModule } from './app/app.module';
-//import { environment } from './environments/environment';
-
-//if (environment.production) {
-  enableProdMode();
-//}
-
-// platformBrowserDynamic()
-//   .bootstrapModule(PokemonModule)
-//   .catch(err => console.error(err));
+import('./bootstrap')
+	.catch(err => console.error(err));
