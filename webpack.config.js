68b5b19d77c192fcc026bbd038e3df813a66ceac
NobilisPerfectum <148438143+NobilisPerfectum@users.noreply.github.com>
2025-08-29 20:31:02 +0530

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+
+module.exports = withModuleFederationPlugin({
+
+  name: 'pokemon',
+
+  exposes: {
+    './PokemonModule': './src/app/pokemon/pokemon.module.ts',
+    './Component': './src/app/app.component.ts',
+  },
+
+  shared: share({
+    "@angular/core": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+    "@angular/common": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+    "@angular/router": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+    ...sharedMappings.getDescriptors()
+  })  
+
+});
