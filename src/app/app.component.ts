f89a130451ff6120549e484330d2fb5b5b9bc78e
NobilisPerfectum <148438143+NobilisPerfectum@users.noreply.github.com>
2025-08-31 17:10:45 +0530

    File Upload 2 [skip ci]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from '@org/core-services';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,12 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = '2pokemon';
+export class AppComponent implements OnInit {
+  title = 'pokemon';
+  constructor(private auth:AuthService){
+    console.log("In pokemon constructor",this.auth.id)
+  }
+  ngOnInit(): void {
+      console.log("IN ngOnInit of pokemon")
+  }
 }
