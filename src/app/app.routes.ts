194b94c9b97205bc92f21191f2f2ebb616400404
NobilisPerfectum <148438143+NobilisPerfectum@users.noreply.github.com>
2025-10-09 12:37:17 +0530

    feat: remove auth routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {
-    path: 'auth-callback',
-    loadComponent: () => import('./auth-callback/auth-callback.component').then(m => m.AuthCallbackComponent)
-  }
+  // {
+  //   path: 'auth-callback',
+  //   loadComponent: () => import('./auth-callback/auth-callback.component').then(m => m.AuthCallbackComponent)
+  // }
 ];
