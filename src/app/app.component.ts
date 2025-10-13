15957c3091ac3500aa9f7b8d3b0c33c2938f7e49
NobilisPerfectum <148438143+NobilisPerfectum@users.noreply.github.com>
2025-10-13 10:27:58 +0530

    fix: check without auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,23 +13,23 @@ import { SalesHistoryComponent } from './sales-history/sales-history.component';
 })
 export class AppComponent implements OnInit {
   title = 'mfe-MY_SALES';
-  isAuthenticated = false;
-  userInfo: any = null;
+  // isAuthenticated = false;
+  // userInfo: any = null;
 
   constructor(private auth: AuthService) {
     console.log("In mfe-MY_SALES constructor", this.auth.id);
   }
 
   ngOnInit(): void {
-    console.log("IN ngOnInit of mfe-MY_SALES");
-    this.isAuthenticated = this.auth.isAuthenticated();
-    this.userInfo = this.auth.getUser();
+    // console.log("IN ngOnInit of mfe-MY_SALES");
+    // this.isAuthenticated = this.auth.isAuthenticated();
+    // this.userInfo = this.auth.getUser();
     
-    // Subscribe to user changes
-    this.auth.user$.subscribe(user => {
-      this.userInfo = user;
-      this.isAuthenticated = !!user;
-    });
+    // // Subscribe to user changes
+    // this.auth.user$.subscribe(user => {
+    //   this.userInfo = user;
+    //   this.isAuthenticated = !!user;
+    // });
   }
 
   login(): void {
